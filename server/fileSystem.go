60a931817141bf76544f306759e17aa0c2baca73
Jeffrey Zhang <jfzhang2@illinois.edu>
2019-11-02 17:14:34 -0500

    Locked in structs we need in the file system file

--- server/fileSystem.go
+++ server/fileSystem.go
@@ -8,39 +8,18 @@ import (
 var FILESYSTEM_TIMEOUT_MS int64 = 6000
 
 // Every heartbeat will also contain a list of all files in the system
-type Requests struct {
-	LastUpdate int64
-	List	   map[string][]string
+type Request struct {
+	Type     string
+	SrcHost  string
+	FileName string
 }
 
-type fileSystem struct {
-	
+// Local datastore that keeps track of the other nodes that have the same files
+type localFiles struct {
+	fileLocations  map[string][]string
+	fileUpdateTime map[string]int64
 }
 
 func FileSystemManager(membership *Membership) {
-	hostname, _ := os.Hostname()
-	ticker := time.NewTicker(1000 * time.Millisecond)
-
-	for {
-		<-ticker.C
-
-		// The leader will be the lowest ID node
-		if membership.List[0] != hostname {
-			continue
-		}
-
-		// Loop through all nodes in the store map. If any of the nodes in the map have timeouts greater than the allowed timeout in the membership map then we need to remove them and figure out where to store the other files.
-		currTime := time.Now().UnixNano() / int64(time.Millisecond)
-		for serverName, _ := range membership.Pending.List {
-
-			lastPing := membership.Data[serverName]
-			if lastPing == 0 || currTime-lastPing > FILESYSTEM_TIMEOUT_MS {
-				// Remove the node from the store and send the file
-			}
-		}
-	}
 }
 
-func rebalanceFileSystem() {
-
-}
