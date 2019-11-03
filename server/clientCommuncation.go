54feefe8b8411f14475e2aa39f1b1c8c520d62a3
Jeffrey Zhang <jfzhang2@illinois.edu>
2019-11-03 15:34:14 -0600

    Worked on resharding and added a new file with some comments

---
+++ server/clientCommuncation.go
@@ -0,0 +1,12 @@
+package server
+
+// This file will handle all communcation with the server
+// So this server will
+
+// goroutine that serverMain will call that will handle accepting TCP connections
+// Needs to handle incoming TCP connections from other nodes as well as incoming connections from the client
+func TcpServerListener() {
+}
+
+// This function should be passed in the membership and filesystem and it will add a request to the Pending Requests list based on the TCP connection it recieved
+func processTCPConnection()
