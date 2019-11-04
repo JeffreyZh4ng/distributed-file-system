b276695bd69cebd8c468565b6623c1b479fcdc27
Xie <zx27@fa19-cs425-g84-02.cs.illinois.edu>
2019-11-04 00:15:49 -0600

    test put

--- client/clientRequest.go
+++ client/clientRequest.go
@@ -6,18 +6,25 @@ import (
 	"net"
 	"io"
 	"encoding/json"
+	"cs-425-mp3/server"
 	"sync"
 	"strconv"
+	"time"
 )
 
 var CLIENT_MSG_PORT string = "23333" // For message 
 var FILE_TRANSFER_PROT string = "43991" // For file
 var FILE_ROOT string = "/home/zx27/SDFS/"
 
-type FileMsg struct{
+type NodeMessage struct {
+	MsgType   string
+	FileName  string
+	SrcHost   string
+	Data      []byte
+}
+type ClientRequest struct{
 	MsgType string
 	FileName string
-	Data []string
 	SrcHost string
 }
 
@@ -33,11 +40,11 @@ func initRequest(requestType string, fileName string) *net.TCPConn{
 			log.Infof("Could not connect to %s, try next", connectName)
 			continue
 		}
-		lsmsg := FileMsg{
+		lsmsg := NodeMessage{
 			MsgType: requestType,
 			FileName: fileName,
-			Data: []string{},
 			SrcHost: localHost,
+			Data: []byte{}
 		}
 		lsmsgbt, err := json.Marshal(lsmsg)
 		clientRequestConn.Write(lsmsgbt)
@@ -64,46 +71,37 @@ func ClientLs(args []string){
 			log.Infof("TCP read error %s", err)
 			return
 		}	
-		newFileMsg := FileMsg{}
-		err = json.Unmarshal(msgbuf[:msglen], &newFileMsg)
+		newNodeMessage := NodeMessage{}
+		err = json.Unmarshal(msgbuf[:msglen], &newNodeMessage)
 		if err != nil {
 			log.Infof("Unable to decode put respond msg %s", err)
 			return
 		}
-		log.Info(newFileMsg.Data)
+		log.Info(newNodeMessage.Data)
 		clientRequestConn.Close()
 }
 
 func ClientGet(args []string){
 	fileName := args[0]
+	
 	clientRequestConn := initRequest("ls_init", fileName)
 	localFilePath := FILE_ROOT + args[1]
-	 
+	go waitingforFile(localFilePath)
+
 	msgbuf := make([]byte, 1024)
+	clientRequestConn.SetReadDeadline(time.Now().Add(time.Second * 5))
 	msglen, err := clientRequestConn.Read(msgbuf)
 	if err != nil {
-		log.Infof("TCP read error %s", err)
+		log.Infof("File is not available")
 		return
 	}	
-	newFileMsg := FileMsg{}
-	err = json.Unmarshal(msgbuf[:msglen], &newFileMsg)
+	newNodeMessage := NodeMessage{}
+	err = json.Unmarshal(msgbuf[:msglen], &newNodeMessage)
 	if err != nil {
 		log.Infof("Unable to decode put respond msg %s", err)
 		return
 	}
-	if newFileMsg.MsgType == "get_ok"{ // fetch file from the source 
-		fConn, err := establishTCP(newFileMsg.Data[0], FILE_TRANSFER_PROT)
-		if err != nil{
-			log.Infof("Could not connect to file source, %s", err)
-			return
-		}
-		TCPFileReceiver(localFilePath, fConn)
-		fConn.Close()
-	}else{
-		log.Infof("The file is unavailable")
-		return
-	}
-	clientRequestConn.Close()
+	defer clientRequestConn.Close()
 
 
 }
@@ -121,32 +119,32 @@ func ClientPut(args []string){
 			log.Infof("TCP read error %s", err)
 			return
 		}	
-		newFileMsg := FileMsg{}
-		err = json.Unmarshal(msgbuf[:msglen], &newFileMsg)
+		newNodeMessage := NodeMessage{}
+		err = json.Unmarshal(msgbuf[:msglen], &newNodeMessage)
 		if err != nil {
 			log.Infof("Unable to decode put respond msg %s", err)
 			return
 		}
-		log.Info(newFileMsg.Data)
+		log.Info(newNodeMessage.Data)
 		clientRequestConn.Close()
-		handleLeaderResponse(newFileMsg, fileName, localHost, localFilePath)	
+		handleLeaderResponse(newNodeMessage, fileName, localHost, localFilePath)	
 		return
 
 }
 
 // For put and ls
-func handleLeaderResponse(responseMsg FileMsg, fileName string, localHost string, localFilePath string){
-	requestMsgTofileMaster := FileMsg{}
+func handleLeaderResponse(responseMsg NodeMessage, fileName string, localHost string, localFilePath string){
+	requestMsgTofileMaster := NodeMessage{}
 	switch responseMsg.MsgType{
 	case "insert_confirm": // There is no file in SDFD now
-		requestMsgTofileMaster = FileMsg{
+		requestMsgTofileMaster = NodeMessage{
 			MsgType: "put",
 			FileName: fileName,
 			Data: responseMsg.Data,
 			SrcHost: localHost,
 		}
 	case "update_confirm": // Client will send update request to fileMaster
-		requestMsgTofileMaster = FileMsg{
+		requestMsgTofileMaster = NodeMessage{
 			MsgType: "put",
 			FileName: fileName,
 			Data: responseMsg.Data,
@@ -178,19 +176,19 @@ func handleLeaderResponse(responseMsg FileMsg, fileName string, localHost string
 		log.Infof("TCP read error %s", err)
 		return
 	}	
-	newFileMsg := FileMsg{}
-	err = json.Unmarshal(msgbuf[:msglen], &newFileMsg)
+	newNodeMessage := NodeMessage{}
+	err = json.Unmarshal(msgbuf[:msglen], &newNodeMessage)
 	if err != nil {
 		log.Infof("Decode error from master %s", err)
 		return
 	}
-	if newFileMsg.MsgType == "put_prompt_ok"{
-		log.Info(newFileMsg.Data)
+	if newNodeMessage.MsgType == "put_prompt_ok"{
+		log.Info(newNodeMessage.Data)
 
 		var wg sync.WaitGroup
 		wg.Add(1)
-		go establishTCPFileServer(&wg, localFilePath, len(newFileMsg.Data))
-		confirmMsg := FileMsg{
+		go establishTCPFileServer(&wg, localFilePath, len(newNodeMessage.Data))
+		confirmMsg := NodeMessage{
 			MsgType: "put_confirmation",
 			FileName: fileName,
 			Data: []string{},
@@ -265,4 +263,44 @@ func TCPFileReceiver(filePath string, readConn *net.TCPConn){
 		return
 	}
 	mkfile.Close()
+}
+
+func waitingforFile(filePath string){
+	tcpAddr := net.ResolveTCPAddr("tcp", ":" + FILE_TRANSFER_PROT)
+	l, err := net.ListenTCP("tcp", tcpAddr)
+	defer l.Close()
+	startTime := time.Now().UnixNano() / int64(time.Millisecond)
+	for{
+		curTime := time.Now().UnixNano() / int64(time.Millisecond)
+		if curTime - startTime > 5000{
+			log.Infof("File unavailable")
+			break
+		}
+		readConn, err := l.AcceptTCP()
+	
+		mkfile, err := os.Create(filePath)
+		if err != nil {
+			log.Infof("Unable to create local file: %s", err)
+			return
+		}
+		io.Copy(mkfile, readConn)
+		if err != nil {
+			log.Infof("Unable to create local file: %s", err)
+			return
+		}
+		mkfile.Close()
+	}	
+}
+
+func sendFile(fileName string, destConn *new.TCPConn){
+	localHost, _ := os.Hostname()
+	loadedFile := ioutil.ReadFile(PARENT_DIR + "/" + fileName)
+	nodeMessage := server.NodeMessage{
+		MsgType: "File",
+		FileName: fileName,
+		SrcHost: localHost,
+		Data: loadedFile,
+	}
+	byteSent, _ := json.Marshal(nodeMessage)
+	destConn.Write(byteSent)
 }
