7a8048f24b984d23e941ac8b9b0ce959ec3b6380
Jeffrey Zhang <jfzhang2@illinois.edu>
2019-11-30 03:10:17 -0600

    Bug fix

--- server/heartbeat.go
+++ server/heartbeat.go
@@ -220,7 +220,7 @@ func processNewMembershipList(newMembership *MembershipList) {
 			sort.Strings(Membership.List)
 
 		// If the new Membership has a more recent time, update it
-		} else if pingTime < newPingTime && abs(pingTime) != newPingTime {
+		} else if pingTime < newPingTime && Abs(pingTime) != newPingTime {
 			Membership.Data[nextHostname] = newPingTime
 
 			// If the hostname is not in the list but it's in the data map,
